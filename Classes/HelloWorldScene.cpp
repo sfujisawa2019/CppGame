0a92dc7969faaa3c928ab84711249ef427145b9b
fujisawa <sfujisawa2019@gmail.com>
2019-11-16 09:54:13 +0900

    練習問題２(TRIANGLE_STRIP)

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -130,23 +130,23 @@ void HelloWorld::draw(Renderer* renderer, const Mat4& transform, uint32_t flags)
 	GL::enableVertexAttribs(GL::VERTEX_ATTRIB_FLAG_POSITION);
 	m_pProgram->use();
 
-	Vec3 pos[6];
-	const float x = 0.2f;
-	const float y = 0.3f;
+	Vec3 pos[4];
+	const float x = 0.7f;
+	const float y = 0.7f;
 
 	// ‚P‚Â–Ú‚ÌŽOŠpŒ`
 	pos[0] = Vec3(-x, -y, 0);
 	pos[1] = Vec3(-x,  y, 0);
 	pos[2] = Vec3( x, -y, 0);
 	// ‚Q‚Â–Ú‚ÌŽOŠpŒ`
-	pos[3] = Vec3(-x, y, 0);
-	pos[4] = Vec3(x, -y, 0);
-	pos[5] = Vec3(x, y, 0);
+	//pos[3] = Vec3(-x, y, 0);
+	//pos[4] = Vec3(x, -y, 0);
+	pos[3] = Vec3(x, y, 0);
 
 	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_POSITION, 3, GL_FLOAT, GL_FALSE, 0, pos);
 
 	// •`‰æ
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
 
 }
